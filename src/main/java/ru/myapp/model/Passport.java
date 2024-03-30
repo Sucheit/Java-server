d9eed28b809a4d79f1b1e2ac6aac350e7fcfa97e
Arthur Inzhilov <artur-inzhilov@mail.ru>
2024-03-31 01:05:49 +0500

    <fix> исправления
    Signed-off-by: Arthur Inzhilov <artur-inzhilov@mail.ru>

--- src/main/java/ru/myapp/model/Passport.java
+++ src/main/java/ru/myapp/model/Passport.java
@@ -11,7 +11,6 @@ import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
