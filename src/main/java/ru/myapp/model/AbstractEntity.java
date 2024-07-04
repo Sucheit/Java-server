8d16254cbc68b76df7338d3fcc60c14794ba6fcb
Arthur Inzhilov <artur-inzhilov@mail.ru>
2024-07-04 16:46:37 +0500

    <fix> исправил equals hashcode для сущностей
    Signed-off-by: Arthur Inzhilov <artur-inzhilov@mail.ru>

--- src/main/java/ru/myapp/model/AbstractEntity.java
+++ src/main/java/ru/myapp/model/AbstractEntity.java
@@ -10,6 +10,7 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 import lombok.experimental.SuperBuilder;
+import org.hibernate.proxy.HibernateProxy;
 
 import java.util.Objects;
 
@@ -27,15 +28,18 @@ public abstract class AbstractEntity {
     protected Integer id;
 
     @Override
-    public boolean equals(Object object) {
-        if (this == object) return true;
-        if (object == null || getClass() != object.getClass()) return false;
-        AbstractEntity that = (AbstractEntity) object;
-        return Objects.equals(id, that.id);
+    public final boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null) return false;
+        Class<?> oEffectiveClass = o instanceof HibernateProxy ? ((HibernateProxy) o).getHibernateLazyInitializer().getPersistentClass() : o.getClass();
+        Class<?> thisEffectiveClass = this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass() : this.getClass();
+        if (thisEffectiveClass != oEffectiveClass) return false;
+        AbstractEntity that = (AbstractEntity) o;
+        return getId() != null && Objects.equals(getId(), that.getId());
     }
 
     @Override
-    public int hashCode() {
-        return Objects.hash(id);
+    public final int hashCode() {
+        return this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass().hashCode() : getClass().hashCode();
     }
 }
