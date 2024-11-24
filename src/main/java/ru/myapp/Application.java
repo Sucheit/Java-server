03e87ae066b4a835e9748899584358efc9cb521b
Arthur Inzhilov <artur-inzhilov@mail.ru>
2024-11-25 01:25:11 +0500

    добавил refresh
    Signed-off-by: Arthur Inzhilov <artur-inzhilov@mail.ru>

--- src/main/java/ru/myapp/Application.java
+++ src/main/java/ru/myapp/Application.java
@@ -1,10 +1,15 @@
 package ru.myapp;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import ru.myapp.config.ActivityToggle;
 
 @EnableAsync
 @EnableScheduling
@@ -16,4 +21,16 @@ public class Application {
 		SpringApplication.run(Application.class, args);
 	}
 
+	@RestController
+	@RequestMapping(path = "activity")
+	@RequiredArgsConstructor
+	public static class ActivitiesRestController {
+
+		private final ActivityToggle activityToggle;
+
+		@GetMapping
+		public Boolean getIsActive() {
+			return activityToggle.getEnabled();
+		}
+	}
 }
--- src/main/java/ru/myapp/config/ActivityToggle.java
+++ src/main/java/ru/myapp/config/ActivityToggle.java
@@ -1,14 +1,17 @@
 package ru.myapp.config;
 
-import lombok.Data;
-import org.springframework.boot.context.properties.ConfigurationProperties;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.stereotype.Component;
 
-@Data
+@Setter
+@Getter
 @Component
 @RefreshScope
-@ConfigurationProperties(prefix = "activity")
+@ConditionalOnProperty(prefix = "activity")
 public class ActivityToggle {
+
     private Boolean enabled;
 }
