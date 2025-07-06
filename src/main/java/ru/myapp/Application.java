c4c0cd91996a0eab386801c81c30fbada2f88570
Arthur Inzhilov <116675812+Sucheit@users.noreply.github.com>
2025-07-06 13:56:48 +0500

    Test (#4)
    * проверка чекстайла
    
    Signed-off-by: Arthur Inzhilov <artur-inzhilov@mail.ru>
    
    * проверка чекстайла
    
    Signed-off-by: Arthur Inzhilov <artur-inzhilov@mail.ru>
    
    * проверка чекстайла
    
    Signed-off-by: Arthur Inzhilov <artur-inzhilov@mail.ru>
    
    * проверка чекстайла
    
    Signed-off-by: Arthur Inzhilov <artur-inzhilov@mail.ru>
    
    ---------
    
    Signed-off-by: Arthur Inzhilov <artur-inzhilov@mail.ru>

--- src/main/java/ru/myapp/Application.java
+++ src/main/java/ru/myapp/Application.java
@@ -9,6 +9,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 public class Application {
 
   public static void main(String[] args) {
-    SpringApplication.run(Application.class, args);
+                SpringApplication.run(Application.class, args);
+    System.out.println("Приложение");
   }
 }
